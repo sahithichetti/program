d07079e7c9db475752f625051e06e3603b7bc98e
sahithichetti <162570201+sahithichetti@users.noreply.github.com>
2024-03-16 19:59:32 +0530

    Add files via upload

---
+++ reverse a linked list.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+# Create a linked list
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+
+print("Original Linked List:")
+llist.print_list()
+
+# Reverse the linked list
+llist.reverse()
+
+print("Reversed Linked List:")
+llist.print_list()
